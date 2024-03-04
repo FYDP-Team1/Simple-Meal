242e6bb8b3bc5cd994d917f7233874cc9c72ea77
InvisCo <20621549+InvisCo@users.noreply.github.com>
2024-03-03 20:43:57 -0500

    Add dumb-init package and unpack images in Dockerfile

--- start-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Unpack the images
-echo "Starting unpacking images..."
-tar -xf recipe_images.tar -C public/recipe_images
-echo "Done unpacking images."
-
-# Start the application
-npm start
