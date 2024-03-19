d433cdd28cfc844ac59b8ccd8d09b6bf98f2ba7e
d269lee <d269lee@uwaterloo.ca>
2024-03-19 10:11:52 -0400

    signup_succeed_page

--- src/pages/SignUpSucceed.js
+++ src/pages/SignUpSucceed.js
@@ -1,6 +1,7 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./SignUpSucceed.module.css";
+import SignUpComponent from "../components/SignUpComponent";
 
 const SignUpSucceed = () => {
   const navigate = useNavigate();
@@ -14,87 +15,96 @@ const SignUpSucceed = () => {
   );
 
   return (
-    <div className={styles.signUpSucceed}>
-      <main className={styles.bannerFrame}>
-        <img
-          className={styles.burgerIcon}
-          loading="eager"
-          alt=""
-          src="/burger@2x.png"
-        />
-        <section className={styles.topBanner}>
-          <img
-            className={styles.burgerIcon1}
-            loading="eager"
-            alt=""
-            src="/burger-1@2x.png"
-          />
-          <img
-            className={styles.eggIcon}
-            loading="eager"
-            alt=""
-            src="/egg@2x.png"
-          />
-          <div className={styles.logoGroup}>
-            <img
-              className={styles.logo}
-              loading="eager"
-              alt=""
-              src="/logo.png"
-            />
+    <div className={styles.signUp}>
+      <img className={styles.burgerIcon} alt="" src="/burger@2x.png" />
+      <div className={styles.burgerIconWrapper}>
+        <div className={styles.burgerIcon1}>
+          <div className={styles.simpleMealLabel}>
+            <div className={styles.signupText}>
+              <div className={styles.eggShape}>
+                <img
+                  className={styles.eggIcon}
+                  loading="eager"
+                  alt=""
+                  src="/egg@2x.png"
+                />
+                <div className={styles.logoGroup}>
+                  <img
+                    className={styles.logo}
+                    loading="eager"
+                    alt=""
+                    src="/logo.png"
+                  />
+                </div>
+              </div>
+              <div className={styles.checkboxContainer}>
+                <img
+                  className={styles.faceBookIcon}
+                  loading="eager"
+                  alt=""
+                  src="/face-book1@2x.png"
+                />
+                <img
+                  className={styles.twitterIcon}
+                  loading="eager"
+                  alt=""
+                  src="/twitter@2x.png"
+                />
+                <img
+                  className={styles.twitterIcon1}
+                  loading="eager"
+                  alt=""
+                  src="/twitter-1@2x.png"
+                />
+                <img
+                  className={styles.youtubIcon}
+                  loading="eager"
+                  alt=""
+                  src="/youtub@2x.png"
+                />
+              </div>
+            </div>
           </div>
           <div className={styles.allRightsReserved}>
             © 2023 All rights reserved
           </div>
-          <img
-            className={styles.faceBookIcon}
-            loading="eager"
-            alt=""
-            src="/face-book@2x.png"
-          />
-          <img
-            className={styles.twitterIcon}
-            loading="eager"
-            alt=""
-            src="/twitter@2x.png"
-          />
-          <img
-            className={styles.twitterIcon1}
-            loading="eager"
-            alt=""
-            src="/twitter-1@2x.png"
-          />
-          <img
-            className={styles.youtubIcon}
-            loading="eager"
-            alt=""
-            src="/youtub@2x.png"
-          />
-          <div className={styles.topBannerChild} />
-          <div className={styles.celebratoryMoment}>
-            <div className={styles.celebratoryMomentChild} />
-            <h1 className={styles.h1}>🎉</h1>
-            <div className={styles.welcomeOnBoardContainer}>
-              <span className={styles.welcomeOnBoardContainer1}>
-                <p className={styles.welcomeOnBoard}>
-                  <b>Welcome on board!</b>
-                </p>
-                <p className={styles.readyToStart}>
-                  Ready to start meal planning?
-                </p>
-              </span>
-            </div>
-            <button
-              className={styles.nextButtonFrame}
-              onClick={onSuccessiveShapeClick}
-            >
-              <div className={styles.successiveShape} />
-              <b className={styles.letsGo}>Let’s go!</b>
-            </button>
+        </div>
+      </div>
+      <div className={styles.burgerParent}>
+      
+        <img
+          className={styles.burgerIcon2}
+          loading="eager"
+          alt=""
+          src="/burger-1@2x.png"
+        />
+        <SignUpComponent />
+      </div>
+
+      <div className={styles.topBannerChild} />
+        <div className={styles.celebratoryMoment}>
+          <div className={styles.celebratoryMomentChild} />
+          <h1 className={styles.h1}>🎉</h1>
+          <div className={styles.welcomeOnBoardContainer}>
+            <span className={styles.welcomeOnBoardContainer1}>
+               <p className={styles.welcomeOnBoard}>
+                <b>Welcome on board!</b>
+              </p>
+              <p className={styles.readyToStart}>
+                Ready to start meal planning?
+              </p>
+            </span>
           </div>
-        </section>
-      </main>
-    </div>
+          <button
+            className={styles.nextButtonFrame}
+            onClick={onSuccessiveShapeClick}
+           >
+             <div className={styles.successiveShape} />
+             <b className={styles.letsGo}>Let’s go!</b>
+           </button>
+         </div>
+      </div>
+    
   );
 };
 
